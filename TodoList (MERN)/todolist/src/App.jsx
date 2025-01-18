1a53225a81549744c59c400eac76cfb647592af5
Mikehope254 <mikehope1738@gmail.com>
2025-01-18 11:35:22 +0300

    Day 1 Todo

---
+++ TodoList (MERN)/todolist/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from "react";
+import "./App.css";
+import Home from "./Home.jsx";
+import Create from "./Create.jsx";
+
+function App() {
+  return (
+    <div>
+      <Home />
+      <Create />
+    </div>
+  );
+}
+
+export default App;
