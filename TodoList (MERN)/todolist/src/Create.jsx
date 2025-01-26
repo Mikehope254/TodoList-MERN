93b51548c4b13b81f849333f24bc2b6fdb8ddf6e
Mikehope254 <mikehope1738@gmail.com>
2025-01-26 15:12:23 +0300

    Completed

--- TodoList (MERN)/todolist/src/Create.jsx
+++ TodoList (MERN)/todolist/src/Create.jsx
@@ -2,15 +2,18 @@ import React, { useState } from 'react'
 import axios from 'axios'
 
 function Create(){
-    const [tast, setTask] = useState()
+    const [tast, setTask] = useState([])
+
     const handleAdd = ()=> {
         if (!task){ //prevents empty tasks from being added
             alert("Please Enter a Task")
             return;
         }
         axios.post('http://localhost:3001/add', {task: task})
-        .then(result => console.log(result))
-            setTask("") //Clear task
+        .then(result => {
+            location.reload()
+        })
+            
         .catch(err => console.log(err))
 
     }
--- TodoList (MERN)/todolist/src/Home.jsx
+++ TodoList (MERN)/todolist/src/Home.jsx
@@ -1,23 +1,55 @@
-import React, {useState} from "react";
+import React, {useEffect, useState} from "react";
 import Create from './Create'
 import './App.css'
 
 
 function Home(){
     const [todos, setTodos] = useState([])
+
+  useEffect(()=>{
+    axios.get('https://localhost:3001/get')
+    .then(result => setTodos(result.data))
+    .catch(err => console.log(err))
+  },[])
+
+  const handleEdit = (id) => {
+    axios.put('https://localhost:3001/update/'+id)
+    .then(result => {
+        location.reload()
+    })
+    .catch(err => console.log(err))
+  }
+
+  const handleDelete = (id) => {
+    axios.delete('https://localhost:3001/delete/'+id)
+    .then(result => {
+        location.reload()
+    })
+    .catch(err => console.log(err))
+  }
     return(
-        <div>
+        <div className="home">
             <h2>ToDo List</h2>
             <Create/>
+            <br/>
             {
                 todos.length === 0 //empty array
                 ?
                 <div><h2>No Record</h2></div>
                 :
                 todos.map(todo=>(
-                   <div>
-                        {todo}
+                   <div className="task">
+                    <div className="checkbox" onClick={()=>handleEdit(todo._id)}>
+                        {todo.done ? 
+                        <BsFillCheckCircleFill className="icon"></BsFillCheckCircleFill>
+                        : <BsCircleFill className="icon"/>}
+                        <p className={todo.done ? "line_through": ""}>{todo.task}</p>   
                     </div>
+                    <div>
+                        <span><BsFillTrashFill className='icon' onClick={() => handleDelete(todo._id)}/></span>
+                    </div>
+                    </div>
+                    
                 ))
             }
         </div>
